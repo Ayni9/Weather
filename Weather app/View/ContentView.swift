6a5e4e48e3fb6ad89bc17e258595e10260d4470a
Renat Kislov <renatkaveev@gmail.com>
2023-06-22 00:03:44 +0500

    Initial Commit

---
+++ Weather app/View/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Weather app
+//
+//  Created by keet on 20.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    @State var weather: ResponseBody?
+    
+    var weatherManager = WeatherManager()
+    
+    var body: some View {
+        VStack {
+            if let location = locationManager.location {
+                if let weather = weather {
+                    WeatherView(weather: weather)
+                } else {
+                    LoadingView()
+                        .task {
+                            do {
+                                weather = try await weatherManager.getCurrentWeather(latitude: location.latitude, longitude: location.longitude)
+                            } catch {
+                                print("Error getting weather: \(error.localizedDescription)")
+                            }
+                        }
+                }
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+        }
+        .background(Color(hue: 0.695, saturation: 0.817, brightness: 0.359))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather app/View/WelcomeView.swift
@@ -0,0 +1,40 @@
+//
+//  WelcomeView.swift
+//  Weather app
+//
+//  Created by keet on 20.06.2023.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("welcome to the weather app")
+                    .bold().font(.title)
+                Text("please share you're current location to get the weather in you're area")
+                    .padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
